0b54a73554db09dd42e6cbd28d5f962f9fa99799
Robeil Aregawi <silukeen1@gmail.com>
2022-04-30 00:35:35 -0500

    added

---
+++ tipCalculator/tip.js
@@ -0,0 +1,13 @@
+function calculateTip() {
+    //getting the subtotal
+    let subtotal = document.getElementById("subTotal").value;
+    //getting percentage
+    let percentage = document.getElementById("percentage").value;
+    
+    //parsing the value to number
+    let total = parseInt(subtotal);
+    let percent = parseInt(percentage);
+
+    document.getElementById("total").innerHTML = (total * (percent / 100)) + parseInt(subtotal);
+}
+
