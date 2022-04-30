a3a7ab2dbe58a760d4f02ca966563f55d205d074
Robeil Aregawi <silukeen1@gmail.com>
2022-04-29 22:55:47 -0500

    added some functionality

--- taskListApp/task.js
+++ taskListApp/task.js
@@ -1,7 +1,3 @@
-// var addTask = document.getElementById("#task").value;
-// var taskList = document.getElementById("#textField");
-// var clearTask = document.getElementsByClassName(".clearBtn");
-
 //adding the given task and store them in localStorage
 function addingTask() {
     //getting the data from the input
@@ -11,12 +7,12 @@ function addingTask() {
     localStorage.setItem(taskKey, mainTask);
     
     //clearing the test field after that
-    document.getElementById("task").value = "Task name";
-    document.getElementById("textField").value = " Write down you tasks HERE!";
+    document.getElementById("task").value = "";
+    document.getElementById("textField").value = "";
 }
 //clearing given task
 function clearTask() {
     localStorage.clear();
-    document.getElementById("task").value = "Task name";
-    document.getElementById("textField").value = " Write down you tasks HERE!";
+    document.getElementById("task").value = "";
+    document.getElementById("textField").value = "";
 }
