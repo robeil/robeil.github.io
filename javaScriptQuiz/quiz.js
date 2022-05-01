6b480364ecbeb33e430030d374c97bb1f4a3f619
Robeil Aregawi <silukeen1@gmail.com>
2022-05-01 17:10:03 -0500

    starting another problem

--- javaScriptQuiz/quiz.js
+++ javaScriptQuiz/quiz.js
@@ -5,5 +5,31 @@ var goBack = document.getElementById("go-back");
 var questionContainer = document.getElementById("question-container");
 var submitBtn = document.getElementById("submit");
 
+document.getElementById("question1").style.visibility = "hidden";
+document.getElementById("question2").style.visibility = "hidden";
+document.getElementById("question3").style.visibility = "hidden";
+document.getElementById("question4").style.visibility = "hidden";
+document.getElementById("thanks").style.visibility = "hidden";
 
-    
+//starting the quiz
+function startQuiz() {
+    document.getElementById("thanks").style.visibility = "hidden";
+    document.getElementById("question1").style.visibility = "visible";
+    document.getElementById("question2").style.visibility = "visible";
+    document.getElementById("question3").style.visibility = "visible";
+    document.getElementById("question4").style.visibility = "visible";
+    console.log("starting the quiz")
+}
+
+//going back to the previous question
+function goingBack(){
+    console.log("going back");
+}
+//finish the quiz
+function finishQuiz(){
+    document.getElementById("thanks").style.visibility = "visible";
+    document.getElementById("question1").style.visibility = "hidden";
+    document.getElementById("question2").style.visibility = "hidden";
+    document.getElementById("question3").style.visibility = "hidden";
+    document.getElementById("question4").style.visibility = "hidden";
+}
