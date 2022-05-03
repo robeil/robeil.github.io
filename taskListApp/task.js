48f98d2a70fa434b2a164af60c2a528102920058
Robeil Aregawi <silukeen1@gmail.com>
2022-05-03 00:14:14 -0500

    adding

--- taskListApp/task.js
+++ taskListApp/task.js
@@ -3,12 +3,13 @@ function addingTask() {
     //getting the data from the input
     var taskKey = document.getElementById("task").value;
     var mainTask = document.getElementById("textField").value;
+    //document.getElementById("textField").innerHTML += mainTask;
     //saving the user input
     localStorage.setItem(taskKey, mainTask);
     
     //clearing the test field after that
     document.getElementById("task").value = "";
-    document.getElementById("textField").value = "";
+    document.getElementById("textField").innerHTML= localStorage.getItem(taskKey);
 }
 //clearing given task
 function clearTask() {
