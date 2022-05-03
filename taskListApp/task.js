4c12cc103249971f50df0b2f170213714f5a0406
Robeil Aregawi <silukeen1@gmail.com>
2022-05-03 10:37:41 -0500

    added

--- taskListApp/task.js
+++ taskListApp/task.js
@@ -1,19 +1,29 @@
 //adding the given task and store them in localStorage
-function addingTask() {
-    //getting the data from the input
-    var taskKey = document.getElementById("task").value;
-    var mainTask = document.getElementById("textField").value;
-    //document.getElementById("textField").innerHTML += mainTask;
-    //saving the user input
-    localStorage.setItem(taskKey, mainTask);
+window.onload = onLoadEvent;
 
-   
-    document.getElementById("textField").value += document.getElementById("task").value + "\n";
-    // document.getElementById("task").value = "";
-}
-//clearing given task
-function clearTask() {
-    localStorage.clear();
-    document.getElementById("task").value = "";
-    document.getElementById("textField").value = "";
+function onLoadEvent() {
+
+    var addBtn = document.getElementById("addBtn");
+    var clearBTn = document.getElementById("clearBtn");
+
+    addBtn.onclick = function addingTask() {
+        //getting the data from the input
+        var taskKey = document.getElementById("task").value;
+        var mainTask = document.getElementById("textField");
+        //document.getElementById("textField").innerHTML += mainTask;
+        //saving the user input
+        localStorage.setItem(taskKey, taskKey);
+        let storedKey = localStorage.getItem(taskKey);
+        console.log(taskKey);
+
+        document.getElementById("textField").value += storedKey + "\n";
+        mainTask.textContent = localStorage.getItem(taskKey);
+        // document.getElementById("task").value = "";
+    }
+    //clearing given task
+    clearBTn.onclick = function clearTask() {
+        localStorage.clear();
+        document.getElementById("task").value = "";
+        document.getElementById("textField").value = "";
+    }
 }
