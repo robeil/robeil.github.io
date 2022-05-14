ec9809d334b0f6fe21b4cd6737feabbbcb877540
Robeil Aregawi <silukeen1@gmail.com>
2022-05-14 01:12:43 -0500

    added

---
+++ Astronomoy(AJAX)/server.js
@@ -0,0 +1,29 @@
+//loading page
+$(document).ready(
+    function () {
+        $("#view_button").click(getPicture);
+    }); 
+//connection
+function getPicture() {
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod",
+        type: "GET",
+        data: {
+            api_key: "Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch",
+            date: $("date").val()
+        },
+        dataType: "json",
+        "success": displayPicture,
+        "error": failedDisplay
+    });
+};
+//displaying picture from the API
+function displayPicture(data) {
+
+    $("#pic").attr("src",data.url);
+    $("#title").text(data.title);
+};
+//error handler
+function failedDisplay(error){
+    alert(error.responseText);
+};
