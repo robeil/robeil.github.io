8850310b1f03ecdd6ed1cb4ad7bd4f8038c24058
Robeil Aregawi <silukeen1@gmail.com>
2022-05-15 12:45:58 -0500

    added

--- Astronomy(Fetch)/server.js
+++ Astronomy(Fetch)/server.js
@@ -1,25 +1,25 @@
 
 $(document).ready(function () {
-    ///using pure promise syntax
+    //using pure promise syntax
     $("#view_button").click(() => {
 
         let date = $("#date").val();
         //sending request here
         fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch`)
-            //getting the promise here    
+            //getting the promise here from the fetch request
             .then(response => response.json())
             //getting and using the response here
             .then(data => {
                 $("#pic").attr("src", data.url);
                 $("#title").text(data.title);
-                $("#dateP").text(data.date);
+                // $("#dateP").text(data.date);
                 $("#explain").text(data.explanation);
             })
             //handling error here if any occur
             .catch(err => console.log(err.message))
 
 
-        //using wait and sync
+        //!using wait and async
       /**   async function usingAsyncAwait() {
             //making the request 
             const resp = await fetch('https://api.nasa.gov/planetary/apod?date=${date}&api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch');
@@ -29,10 +29,8 @@ $(document).ready(function () {
             $("#pic").attr("src", data.url);
             $("#title").text(data.title);
         }
-        //calling the function
-
+        //!calling the function async and await
         usingAsyncAwait();
         */
     })
-
 })
