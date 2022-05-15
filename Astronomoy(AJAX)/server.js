157276e5823e59cf36aaf97e8274a3b630c78d75
Robeil Aregawi <silukeen1@gmail.com>
2022-05-14 21:53:27 -0500

    added

--- Astronomoy(AJAX)/server.js
+++ Astronomoy(AJAX)/server.js
@@ -1,16 +1,16 @@
 //loading page
-$(document).ready(
-    function () {
-        $("#view_button").click(getPicture);
-    });
+$(document).ready(() => {
+    $("#view_button").click(getPicture);
+});
 //connection
 function getPicture() {
+    let date = $("#date").val();
     $.ajax({
-        url: "https://api.nasa.gov/planetary/apod?api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch",
+        url: `https://api.nasa.gov/planetary/apod?date=${date}`,
         type: "GET",
         data: {
-            //api_key: "Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch",
-            date: $("date").val()
+            api_key: "Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch"
+            // date: $("date").val()
         },
         dataType: "json",
         "success": displayPicture,
@@ -19,9 +19,9 @@ function getPicture() {
 };
 //displaying picture from the API
 function displayPicture(data) {
-
     $("#pic").attr("src", data.url);
     $("#title").text(data.title);
+    $("#explain").text(data.explanation);
 };
 //error handler
 function failedDisplay(error) {
--- Astronomy(Fetch)/server.js
+++ Astronomy(Fetch)/server.js
@@ -1,27 +1,38 @@
-///using pure promise syntax
-//sending request here
-fetch('https://api.nasa.gov/planetary/apod?api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch')
-    //getting the promise here    
-    .then(response => response.json())
-    //getting and using the response here
-    .then(data => {
-        $("#pic").attr("src", data.url);
-        $("#title").text(data.title);
+
+$(document).ready(function () {
+    ///using pure promise syntax
+    $("#view_button").click(() => {
+
+        let date = $("#date").val();
+        //sending request here
+        fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch`)
+            //getting the promise here    
+            .then(response => response.json())
+            //getting and using the response here
+            .then(data => {
+                $("#pic").attr("src", data.url);
+                $("#title").text(data.title);
+                $("#dateP").text(data.date);
+                $("#explain").text(data.explanation);
+            })
+            //handling error here if any occur
+            .catch(err => console.log(err.message))
+
+
+        //using wait and sync
+      /**   async function usingAsyncAwait() {
+            //making the request 
+            const resp = await fetch('https://api.nasa.gov/planetary/apod?date=${date}&api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch');
+            //getting the data back  and store
+            const data = await resp.json();
+            //using the received data for our page
+            $("#pic").attr("src", data.url);
+            $("#title").text(data.title);
+        }
+        //calling the function
+
+        usingAsyncAwait();
+        */
     })
-    //handling error here if any occur
-    .catch(err => console.log(err.message))
 
-//using wait and sync
-async function usingAsyncAwait() {
-    //making the request 
-    const resp = await fetch('https://api.nasa.gov/planetary/apod?api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch');
-    //getting the data back  and store
-    const data = await resp.json();
-    //using the received data for our page
-    $("#pic").attr("src", data.url);
-    $("#title").text(data.title);
-}
-
-//calling the function
-
-//usingAsyncAwait();
+})
